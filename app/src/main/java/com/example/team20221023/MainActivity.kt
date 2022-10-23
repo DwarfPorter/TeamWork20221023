e5fe7ead4450f6cd41fb6b06768f8877945bb701
DwarfPorter <mrvlmor@gmail.com>
2022-10-23 21:49:13 +0300

    Новое приветствие

--- app/src/main/java/com/example/team20221023/MainActivity.kt
+++ app/src/main/java/com/example/team20221023/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.team20221023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.text)
+        text.text = "Привет!"
     }
 }
